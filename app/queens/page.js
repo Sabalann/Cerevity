86ce7ed1ca99630059c89c33f980b50d87acde0a
Sabalan Alaeiyeh <s.alaeiyeh@students.uu.nl>
2025-01-07 21:40:38 +0100

    added queens page and queens board

---
+++ app/queens/page.js
@@ -0,0 +1,10 @@
+import './queens.css'
+
+export default function QueensPage() {
+    return (
+      <div className='content-center h-full'>
+        <div className="queensBoard">
+        </div>
+      </div>
+    );
+  }
