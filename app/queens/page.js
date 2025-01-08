d68d303d2aebc6ce4a9ed40fc3bedc02db49e551
Sabalan Alaeiyeh <s.alaeiyeh@students.uu.nl>
2025-01-08 13:19:54 +0100

    added buttons, borderbackground styling, 3 fonts to play around with

--- app/queens/page.js
+++ app/queens/page.js
@@ -2,8 +2,16 @@ import './queens.css'
 
 export default function QueensPage() {
     return (
-      <div className='content-center h-full'>
-        <div className="queensBoard">
+      <div className='justify-items-center h-full pt-36'>
+        <div className='game-container'>
+          <div className='borderBackground mb-4'>
+            <div className="queensBoard">
+            </div>
+          </div>
+          <div className='w-full flex justify-between'>
+            <div className='borderBackground'><button className='button'>Start</button></div>
+            <div className='borderBackground'><button className='button'>Hint</button></div>
+          </div>
         </div>
       </div>
     );
